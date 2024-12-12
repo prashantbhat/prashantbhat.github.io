f36646eef2246913f25e843e904263f1308d0c8c
Prashant Bhat <prashant.mr@gmail.com>
2024-12-12 10:09:14 +0530

    add bulma theme selector menu

--- js/bulma.js
+++ js/bulma.js
@@ -19,3 +19,85 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+// THEMES
+const STORAGE_KEY = "bulma-theme";
+const SYSTEM_THEME = "system";
+const DEFAULT_THEME = "light";
+
+const state = {
+  chosenTheme: SYSTEM_THEME, // light|dark|system
+  appliedTheme: DEFAULT_THEME, // light|dark
+  OSTheme: null, // light|dark|null
+};
+
+const updateThemeUI = () => {
+  const isLightTheme = state.appliedTheme === "light";
+  const themeIcon = document.getElementById("theme-icon");
+  themeIcon.classList.remove("has-text-link", "has-text-warning");
+  themeIcon.classList.add(isLightTheme ? "has-text-warning" : "has-text-link");
+  const themeIconHtml = "<i class=\"fas fa-sun\"></i>"
+  themeIcon.children[0].innerHTML = isLightTheme ? themeIconHtml : themeIconHtml.replace("sun", "moon");
+};
+
+const setTheme = (theme, save = true) => {
+  state.chosenTheme = theme;
+  state.appliedTheme = theme;
+  if (theme === SYSTEM_THEME) {
+    state.appliedTheme = state.OSTheme;
+    document.documentElement.removeAttribute("data-theme");
+    window.localStorage.removeItem(STORAGE_KEY);
+  } else {
+    document.documentElement.setAttribute("data-theme", theme);
+    if (save) {
+      window.localStorage.setItem(STORAGE_KEY, theme);
+    }
+  }
+  updateThemeUI();
+};
+
+const toggleTheme = () => {
+  if (state.appliedTheme === "light") {
+    setTheme("dark");
+  } else {
+    setTheme("light");
+  }
+};
+
+const detectOSTheme = () => {
+  if (!window.matchMedia) {
+    // matchMedia method not supported
+    return DEFAULT_THEME;
+  }
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    // OS theme setting detected as dark
+    return "dark";
+  } else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+    return "light";
+  }
+  return DEFAULT_THEME;
+};
+
+// On load, check if any preference was saved
+window.addEventListener("DOMContentLoaded", () => {
+  const localTheme = window.localStorage.getItem(STORAGE_KEY);
+  state.OSTheme = detectOSTheme();
+  if (localTheme) {
+    setTheme(localTheme, false);
+  } else {
+    setTheme(SYSTEM_THEME);
+  }
+  // Event listeners
+  const $themeSwitchers = document.querySelectorAll(".js-themes");
+  $themeSwitchers.forEach((el) => {
+    el.addEventListener("click", () => {
+      setTheme(el.dataset.scheme);
+    });
+  });
+});
+
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (event) => {
+  const theme = event.matches ? "dark" : "light";
+  state.OSTheme = theme;
+  setTheme(theme);
+});
